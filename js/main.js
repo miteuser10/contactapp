80ebe7f6787ae2957ec7c3c8ac2859c210f36e26
unknown <akshithagshettigar@gmail.com>
2018-07-18 13:01:01 +0530

    created my beautified contact page

--- js/main.js
+++ js/main.js
@@ -1,60 +1,68 @@
-var contacts = [
-    {"name": "Ganesh", "email": "ganeshshettigar@gmail.com", "mobile" : "29383929"},
-    {"name": "Lola", "email": "lolakshishettigar@gmail.com", "mobile" : "29383933"},
-    {"name": "Prapthi", "email": "prapthishettigar@gmail.com", "mobile" : "2938392"}
-    
-];
+if (localStorage.getItem("contacts") == null)
+    localStorage.setItem("contacts", JSON.stringify([]));
 var tempIndex = -1;
 
 function addContact() {
-    contact = getContact();
+    var contact = getContact();
+    var contacts = getDataFromLocalstorage();
     contacts.push(contact);
+    updateDataToLocalStorage(contacts);
     clearFormData();
     viewData();
-    
+
 }
 
 function viewData() {
+    var contacts = getDataFromLocalstorage();
     var data = "";
+    if (contacts.length == 0)
+        data = "Contacts are not yet added!";
+    else{
     data += "<table>";
     for (i = 0; i < contacts.length; i++) {
         data += "<tr>";
         data += "<td>" + contacts[i].name + "</td>";
         data += "<td>" + contacts[i].email + "</td>";
         data += "<td>" + contacts[i].mobile + "</td>";
-        data += "<td>" +"<button onclick=editContact("+ i +")>Edit</button>"+ "</td>"
-        data += "<td>" +"<button onclick=deleteContact("+ i +")>Delete</button>"+ "</td>"
+        data += "<td>" + "<button onclick=editContact(" + i + ")>Edit</button>" + "</td>"
+        data += "<td>" + "<button onclick=deleteContact(" + i + ")>Delete</button>" + "</td>"
         data += "</tr>";
     }
     data += "</table";
+    }
     document.getElementById("content").innerHTML = data;
 }
 
 viewData();
 
 function editContact(index) {
-    contact = contacts[index];
+    var contacts = getDataFromLocalstorage();
     tempIndex = index;
     document.getElementById('cname').value = contact.name;
     document.getElementById('email').value = contact.email;
     document.getElementById('mobile').value = contact.mobile;
-    
-    document.getElementById('add').style.display="none";
-    document.getElementById('update').style.display="block";
-    
+
+    document.getElementById('add').style.display = "none";
+    document.getElementById('update').style.display = "block";
+
 }
 
 function deleteContact(index) {
-   contacts.splice(index,1);
+     var contacts = getDataFromLocalstorage();
+    contacts.splice(index, 1);
+     updateDataToLocalStorage(contacts);
     viewData();
 }
 
 function updateContact() {
+    var contacts = getDataFromLocalstorage();
     contact = getContact();
-    contacts.splice(tempIndex,1,contact);
+    
+    contacts.splice(tempIndex, 1, contact);
+    updateDataToLocalStorage(contacts);
     clearFormData();
-    document.getElementById("add").style.display="block";
-    document.getElementById("update").style.display="none";
+    document.getElementById("add").style.display = "block";
+    document.getElementById("update").style.display = "none";
     viewData();
 }
 
@@ -63,14 +71,24 @@ function clearFormData() {
     document.getElementById("email").value = "";
     document.getElementById("mobile").value = "";
 }
+
 function getContact() {
     var cname = document.getElementById('cname').value;
     var email = document.getElementById('email').value;
     var mobile = document.getElementById('mobile').value;
     contact = {
-        "name" : cname,
-        "email" : email,
-        "mobile" : mobile
+        "name": cname,
+        "email": email,
+        "mobile": mobile
     }
     return contact;
+}
+
+function getDataFromLocalstorage() {
+    contacts = JSON.parse(localStorage.getItem("contacts"));
+    return contacts;
+}
+
+function updateDataToLocalStorage(updatedData) {
+    localStorage.setItem("contacts", JSON.stringify(updatedData));
 }
